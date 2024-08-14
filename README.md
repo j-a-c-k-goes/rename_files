f6619b8463480bdb7235562665098c18efd92431
xyz <jack@flnpb.com>
2024-08-14 17:39:14 -0400

    WriteLog function added, script tested as working, README updated
    Signed-off-by: xyz <jack@flnpb.com>

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 This script has one job. Rename files that contain spaces. 
 Renaming renaming makes looking for files a more convenient process. It will walk from the root path and traverse the entire filesystem (run in background for silence). 
 
-*FYI: Walking from / will change names of all files found. This may break things. The script runs in dry mode by default.*
+*FYI: Walking from / will change names of all files found. This may break things. The script runs in dry mode by default. It is probably better to not walk from root. And it is even better to target specific directories.*
 
 ### How Files Are Renamed?
 * Directories renamed with an underscore, `_`.
@@ -24,14 +24,20 @@ You have too many files on your system named like "my file has spaces".
 
 ## Running the script
 * One-time use
-    - `python rename_files.py <dry-run-on|dry-run-off`
+    - `python rename_files.py <dry-run-on|dry-run-off <path>`
 * Periodic use
     - Setup a cronjob `crontab -e`
     - Set script to run peridically (every monday, for example)
-        + (from crontab) `* * * * mon python rename_files.py <dry-run-on|dry-run-off>`
+        + (from crontab) `* * * * mon python rename_files.py <dry-run-on|dry-run-off> <path>`
+    - Clean up periodically. Remove logs older than 30days every 15th day @ 10:30am
+        + (from crontab) `30 10 15 * * find /tmp/*-log-*.log -mtime +30 -exec rm {} \;`
 *Running in dry-run-on mode allows you to view what would be renemaed without actually having the process take place. Try before you buy. Also: dry-run does not generate a log file.*
 
-## Checking the Logs
-The script creates a log file to display a few stats on what happened during the script run. This file lives at: `/tmp/file-renaming.log`
+## Examples
+* Rename all files in the desktop directory
+    - `python rename_files.py dry-run-on $HOME/Desktop`
 
-To display its contents, run `cat /tmp/file-renaming.log`
+## Checking the Logs
+The script creates a log file to display a few stats on what happened during the script run. This files are: `/tmp/<timestamp>-log-files-renamed.log`, `/tmp/<timestamp>-log-directories-renamed.log`
+
+To display logged files, run `cat /tmp/*-log-*-*.log`
--- rename_files.py
+++ rename_files.py
@@ -2,97 +2,133 @@
 '''
 import sys
 import os
+import datetime
+#---------------------------------------------------------------------------
+month    = datetime.datetime.now().month
+year     = datetime.datetime.now().year
+day      = datetime.datetime.now().day
+hour     = datetime.datetime.now().hour
+minute   = datetime.datetime.now().minute
 #---------------------------------------------------------------------------
 def Msg(type, msg):
-   print(f'[{type}]   {msg}')
+   print(f'[{type}]    {msg}')
 #---------------------------------------------------------------------------
 def Usage():
-   print("[fix]     Specify whether or not to run the script in dry-run mode.")
-   print("[usage]   <script-name> <dry-run-on|dry-run-off>")
-   print("[example] rename_files.py dry-run-on\n")
+   print(f'[fix]        Specify whether or not to run the script in dry-run mode.')
+   print(f'[usage]      <script-name> <dry-run-on|dry-run-off> <some-path>')
+   print(f'[example]    rename_files.py dry-run-on /home\n')
 #---------------------------------------------------------------------------
 def CheckArgs():
-   arg_check_passed = bool()
-   if len(sys.argv) < 2:
-      Msg('error', 'Not enough arguments.')
-      arg_check_passed = False   
-      Usage()
-   else:
-      Msg('update', 'Args present. Checking for correctness.')
-      arg_to_check   = sys.argv[1]
-      DRY_RUN_ON     = arg_to_check.lower() == 'dry-run-on'
-      DRY_RUN_OFF    = arg_to_check.lower() == 'dry-run-off'
-      valid_arg      = (DRY_RUN_ON) or (DRY_RUN_OFF)
-      if valid_arg:
-         arg_check_passed = True
-         return { 'status': arg_check_passed, 'mode': arg_to_check }
-      else:
-         Msg('error', f'Invalid argument "{arg_to_check}".')
-         arg_check_passed = False
-         Usage()
-   return { 'status': arg_check_passed, 'mode': None }
-#---------------------------------------------------------------------------
-def LookForNamingViolations(mode:str):
-   files_in_naming_violation        = 0
-   directories_in_naming_violation  = 0
-   offending_directories            = list()
-   offending_files                  = list()
-   print(f'[mode] {mode}\n')
-   print("Preparing to walk from home, '/'")
    try:
-      for root, directories, files in os.walk('/', topdown=True, followlinks=True):
-         for directory in directories:
-            naming_violation = (' ' in directory)
-            if naming_violation:
-               directories_in_naming_violation += 1
-               offending_directories.append(directory)
-               prep_to_fix = directory.split()
-               naming_fix  = '_'.join(prep_to_fix)      
-               print(f'Renaming {root}/{directory} -> {root}/{naming_fix}')
-               if (mode == 'dry-run-on'):
-                  pass
-               elif (mode =='dry-run-off'):
-                  #os.system(f'mv {root}/{directory} -> {root}/{naming_fix}')
-                  pass
-         for file in files:
-            naming_violation = (' ' in file)
-            if naming_violation:
-               files_in_naming_violation += 1
-               offending_files.append(file)
-               prep_to_fix = file.split()
-               naming_fix  = '-'.join(prep_to_fix)
-               print(f'Renaming {root}/{file} -> {root}/{naming_fix}')
-               if (mode == 'dry-run-on'):
-                  pass
-               elif (mode =='dry-run-off'):
-                  #os.system(f'mv {root}/{file} -> {root}/{naming_fix}')
-                  pass
-      return { 
-         'naming_violations': 
-         {
-            'files': files_in_naming_violation, 
-            'directories':  directories_in_naming_violation
-         },
-         'list_of_offenders': 
-         {
-            'files': offending_files,
-            'directories': offending_directories
-         }
-      }
+      arg_check_passed = bool()
+      if len(sys.argv) < 3:
+         Msg('error', 'Not enough arguments.')
+         arg_check_passed = False   
+         Usage()
+      else:
+         Msg('update', 'Args present. Checking for correctness.')
+         args_to_check  = [ sys.argv[1], sys.argv[2] ]
+         DRY_RUN_ON     = args_to_check[0].lower() == 'dry-run-on'
+         DRY_RUN_OFF    = args_to_check[0].lower() == 'dry-run-off'
+         valid_mode     = (DRY_RUN_ON) or (DRY_RUN_OFF)
+         valid_path     = os.path.exists(args_to_check[1])
+         if (valid_mode and valid_path):
+            arg_check_passed = True
+            return { 'status': arg_check_passed, 'mode': args_to_check[0], 'path': args_to_check[1] }
+         else:
+            if (valid_mode == False):
+               Msg('error', f'Invalid mode "{args_to_check[0]}".')
+            if (valid_path == False):
+               Msg('error', f'Non-existing path "{args_to_check[1]}".')
+            arg_check_passed = False
+            Usage()
+      return { 'status': arg_check_passed, 'mode': None, 'path': None }
+   except FileNotFoundError as exception:
+      Msg('exception', f'{sys.argv[2]} is not a vaild path')
+      print(exception)
+   except Exception as exception:
+      Msg('exception','See exception message.')
+      print(exception)
+#---------------------------------------------------------------------------
+def LookForNamingViolations(path, mode:str):
+   try:
+      files_in_naming_violation        = 0
+      directories_in_naming_violation  = 0
+      tmp_log_statements_files         = list()
+      tmp_log_statements_dirs          = list()
+      log_statements                   = { 'files': list(), 'directories': list() }
+      print(f'[mode] {mode}\n')
+      if (not os.path.exists(path)):
+         Msg('error', f'Cannot walk non-existing path ({path})')
+         exit(-1)
+      else:
+         print(f'Preparing to walk from "{path}"')
+         for root, directories, files in os.walk(f'{path}', topdown=True, followlinks=True):
+            for directory in directories:
+               naming_violation = (' ' in directory)
+               if naming_violation:
+                  directories_in_naming_violation += 1
+                  prep_to_fix    = directory.split()
+                  naming_fix     = '_'.join(prep_to_fix)      
+                  log_statement  = (f'Renamed {root}/{directory} -> {root}/{naming_fix}')
+                  if (mode == 'dry-run-on'):
+                     print(log_statement)
+                  elif (mode =='dry-run-off'):
+                     os.system(f'mv {root}/{directory} -> {root}/{naming_fix}')
+                     tmp_log_statements_dirs.append(log_statement)
+            for file in files:
+               naming_violation = (' ' in file)
+               if naming_violation:
+                  files_in_naming_violation += 1
+                  prep_to_fix    = file.split()
+                  naming_fix     = '-'.join(prep_to_fix)
+                  log_statement  = (f'Renamed {root}/{file} -> {root}/{naming_fix}')
+                  if (mode == 'dry-run-on'):
+                     print(log_statement)
+                  elif (mode =='dry-run-off'):
+                     os.system(f'mv {root}/{file} -> {root}/{naming_fix}')
+                     tmp_log_statements_files.append(log_statement)
+                     pass
+         
+         WriteLog(f'/tmp/{year}{month}{day}{hour}{minute}-log-files-renamed.log', mode, tmp_log_statements_files)
+         WriteLog(f'/tmp/{year}{month}{day}{hour}{minute}-log-directories-renamed.log', mode, tmp_log_statements_dirs)
+         Msg('files-renamed', files_in_naming_violation)
+         Msg('directories-renamed', directories_in_naming_violation)
+         Msg('total', (files_in_naming_violation+directories_in_naming_violation))
    except Exception as error:
       print(error)
       return -1
 #---------------------------------------------------------------------------
-# while open('/tmp/file-renaming.log', 'w') as source_file:
-#    source_file.write('...')
+def WriteLog(file_name, mode, log_statements:list):
+   try:
+      with open(file_name, 'w') as source_file:
+         source_file.write('---\n')
+         source_file.write(f'TIMESTAMP {year}/{month}/{day} @ {hour}:{minute}\n')
+         source_file.write(f'ran {sys.argv[0]} in {mode} mode\n'.upper())
+         source_file.write('---\n')
+         if len(log_statements) < 1:
+            source_file.write('No log statements to report.\n')
+         if (mode == 'dry-run-on'):
+            source_file.write('There is no logging when DRY-RUN-ON\n')
+         else:
+            for index, log_statement in enumerate(log_statements):
+               source_file.write(f'[{index}]    {log_statement}\n')
+         print(f'Done writing {file_name}.')
+   except FileNotFoundError as fnf_exception:
+      Msg('exception', f'{file_name} not found. Check path or spelling.')
+      print(fnf_exception)
+   except Exception as general_exception:
+      Msg('exception', 'Consult error message below for details.')
+      print(general_exception)
 #---------------------------------------------------------------------------
 if __name__ == '__main__':
    arg = CheckArgs()
    try:
       if arg.get('status') == True:
+         path_to_walk = arg.get('path')
          mode = arg.get('mode')
          Msg('update', 'Can proceed with checking for naming violations.')
-         LookForNamingViolations(mode)
+         LookForNamingViolations(path_to_walk, mode)
       else:
          exit(-1)
    except Exception as error:
